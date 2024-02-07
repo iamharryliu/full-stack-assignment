0b317a9103a8cd3ceb3e7a98d8a19d68f8b55476
Harry Liu <harryliu1995@gmail.com>
2024-02-07 13:30:45 +0100

    feat: add table filtering

--- demo-ui/src/App.js
+++ demo-ui/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { Table, Button } from 'react-bootstrap';
+import { Table, Button, FormControl } from 'react-bootstrap';
 
 function App() {
   const [customers, setCustomers] = useState([]);
@@ -16,6 +16,9 @@ function App() {
 
   const [sortField, setSortField] = useState(null);
   const [sortOrder, setSortOrder] = useState('asc');
+  const [filterField, setFilterField] = useState(null);
+  const [filterValue, setFilterValue] = useState('');
+  const [activeFilter, setActiveFilter] = useState(null);
 
   const handleSort = field => {
     if (sortField === field) {
@@ -26,7 +29,24 @@ function App() {
     }
   };
 
-  const sortedCustomers = [...customers].sort((a, b) => {
+  const handleFilter = field => {
+    if (activeFilter === field) {
+      setFilterField(null);
+      setFilterValue('');
+      setActiveFilter(null);
+    } else {
+      setFilterField(field);
+      setActiveFilter(field);
+    }
+  };
+
+  const filteredCustomers = filterField
+    ? customers.filter(customer =>
+        customer[filterField].toLowerCase().includes(filterValue.toLowerCase()),
+      )
+    : customers;
+
+  const sortedCustomers = [...filteredCustomers].sort((a, b) => {
     if (sortField) {
       const aValue = a[sortField];
       const bValue = b[sortField];
@@ -39,7 +59,40 @@ function App() {
   });
 
   return (
-    <div className="App">
+    <div class="container">
+      <h1 className="text-center mb-4">AdventureWorks Sort Filter Customers</h1>
+      <div className="mb-4">
+        <FormControl
+          type="text"
+          placeholder="Filter by value..."
+          className="mb-2"
+          value={filterValue}
+          onChange={e => setFilterValue(e.target.value)}
+        />
+        <Button
+          variant={activeFilter === 'firstName' ? 'primary active' : 'primary'}
+          onClick={() => handleFilter('firstName')}
+          className="me-3"
+        >
+          Name
+        </Button>
+        <Button
+          variant={
+            activeFilter === 'emailAddress' ? 'primary active' : 'primary'
+          }
+          onClick={() => handleFilter('emailAddress')}
+          className="me-3"
+        >
+          Email
+        </Button>
+        <Button
+          variant={activeFilter === 'phone' ? 'primary active' : 'primary'}
+          onClick={() => handleFilter('phone')}
+          className="me-3"
+        >
+          Phone
+        </Button>
+      </div>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -68,7 +121,6 @@ function App() {
                 Phone
               </Button>
             </th>
-            {/* Add more columns as needed */}
           </tr>
         </thead>
         <tbody>
